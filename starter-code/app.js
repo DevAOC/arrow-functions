a817dbe63705a3b831e099a54f35a34ba64d2103
AntoineCharette <antoineocharette@gmail.com>
2021-08-14 15:46:55 -0400

    Step 3 complete (Prettier extension adds parens to my code automatically... have not added exceptions in a JSON file sorry)

--- starter-code/app.js
+++ starter-code/app.js
@@ -17,7 +17,7 @@ const theNewWay = (course) => {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-console.log("The new way:", theNewWay("Code 301"));
+// console.log("The new way:", theNewWay("Code 301"));
 
 // STEP 3
 // When we have one parameter, we can omit the parentheses
@@ -26,7 +26,7 @@ const withoutParens = (course) => {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('Without parens:', withoutParens('Code 301'));
+console.log("Without parens:", withoutParens("Code 301"));
 
 // STEP 4
 // If the code block contains a single line of code, we can write everything on one line
